22467219c9b805a02b6ba1d7c17ae711f1354d70
Turtle <anuragdev12@gmail.com>
2024-02-26 13:40:53 +0530

    add basic zod validate

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,29 @@
 const express = require('express');
+const { createTodo, updateTodo } = require('./types.js');
 const app = express();
+
+app.use(express.json());
+
+app.post('/todo', (req, res) => {
+    const createPayLoad = req.body;
+    const parsePayLoad = createTodo.safeParse(createPayLoad);
+    if (!parsePayLoad.success) {
+        res.status(404).json({ msg: 'Wrong Inputs!' });
+        return;
+    }
+
+    //send to database
+});
+
+app.get('/todos', (req, res) => {});
+
+app.put('/completed', (req, res) => {
+    const updatePayLoad = req.body;
+    const parsePayLoad = updateTodo.safeParse(updatePayLoad);
+    if (!parsePayLoad.success) {
+        res.status(404).json({ msg: 'Wrong Inputs!' });
+        return;
+    }
+
+    //send to database
+});
